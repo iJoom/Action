6c1746d54e3e3ec09995ee46a82e8437f9506090
iJoom <ghfdld@naver.com>
2021-12-04 23:01:04 +0900

    create

---
+++ My/My/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My
+//
+//  Created by IJ . on 2021/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
